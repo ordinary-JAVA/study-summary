317b8eb1a6133e7a6d86bc18ee376c02751988ad
ordinary-JAVA <lizongren20@163.com>
2020-12-15 19:40:38 +0800

    12.15提交

---
+++ src/main/java/com/example/test/bean/Test.java
@@ -0,0 +1,11 @@
+package com.example.test.bean;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        float a=1.0F-0.3F;
+        float b=2.0F-1.3F;
+        System.out.println(a==b);
+    }
+}
